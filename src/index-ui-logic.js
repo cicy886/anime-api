4b3bbe0ce03bb06345d99f16a51743accb74e89d
Sisi Vieira <cicy886@gmail.com>
2021-08-09 20:43:01 -0700

    new class practice

--- src/index-ui-logic.js
+++ src/index-ui-logic.js
@@ -2,6 +2,18 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import InspirationQuote from './business-logic.js';
 
 $(document).ready(function() {
+  $('#quoteButton').click (function(){
+    let promise = InspirationQuote.getQuote();
+    promise.then(function(response){
+      const body = JSON.parse(response);
+      $('.showQuote').text(`${body.affirmation}`);
+      $('.showQuote').text('');
+      // $('.showAuthor').text(`${body.author}`);
+    }, function(error){
+      $('.showError').text(`There was an error message: ${error}`);
+    });
+  });
 });
